82e287fd8fc1750fa8c0a0d2799137ba3b662619
Ysaak <ysaakescolaluno@gmail.com>
2024-03-21 20:01:45 -0300

    Modificação feita

--- src/sistema/Arrays.java
+++ src/sistema/Arrays.java
@@ -1,5 +1,19 @@
 package sistema;
 
+
+
 public class Arrays {
 
+	public static void main(String[] args) {
+		double i,j;
+		
+		for(i = 0 ; i<=3;i++) {
+			for(j= 1;j<=3;j++) {
+				System.err.print(i+1+" | "+j+1);
+			}
+		}
+		
+		
+		//
+	}
 }
